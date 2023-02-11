4687964540ef6428734b2eb9df81e3c2750aa811
Monika <l73583@126.com>
2023-02-11 17:25:55 +0800

    add clearrootfs

--- grfd.sh
+++ grfd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd rootfs
 mkdir -p dev etc home lib lib64 mnt proc root sys tmp var
-ln -sf ../lib usr/lib
-ln -sf ../lib64 usr/lib64
+ln -sf ../lib rootfs/usr/lib
+ln -sf ../lib64 rootfs/usr/lib64
 cd ..
 cp -r bee/* rootfs
